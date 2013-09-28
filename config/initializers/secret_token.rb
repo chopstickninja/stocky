d6bc525a383ed3e2674373fa95b3869ba2e42779
Jenny Chou <j.3h15u@gmail.com>
2013-09-28 14:48:28 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stocky::Application.config.secret_token = '024a271e338f9fd784d12798fd0a28f08519ca320eb12b13542b7266ad34d6552ff59cf276fa6bc50d9d61a5dedefd2b20f9fb047bb2e9177a858e320551819e'
