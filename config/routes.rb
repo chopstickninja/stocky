eae0f2dfaeb3b8d6aa6ec5dd1049a57201c9a4e3
Praveen Chekuri <praveenchek@gmail.com>
2013-09-29 07:41:09 -0700

    fix more errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Stocky::Application.routes.draw do
 
   resources :strategies, :only => [:show, :index, :create, :update, :destroy]
-end
 
   resources :users, :only => [:create, :index, :new]
   resource :session
