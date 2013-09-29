6db0e3cdabece97ece59a0616aad67e9238e9c02
Praveen Chekuri <praveenchek@gmail.com>
2013-09-29 11:22:56 -0700

    pushed stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20130929140815) do
+ActiveRecord::Schema.define(:version => 20130929175530) do
 
   create_table "strategies", :force => true do |t|
     t.string   "query"
