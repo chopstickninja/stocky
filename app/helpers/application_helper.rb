b66bc46af7ec4307c2b8b92690942731540cca18
Jenny Chou <j.3h15u@gmail.com>
2013-09-29 05:15:33 -0700

    FOR clause treatment in find_ranges

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -87,48 +87,75 @@ module ApplicationHelper
     # =>
     # when ticker change by increment per duration for length
 
-    def get_prices(ticker, duration_examined)
-      yql = Yql::Client.new
-      yql.format = "json"
-      daily_data = [] #daily prices IPO - now
-      res = [0] #temp holder for year's prices
-      endDate = DateTime.now
-      query = Yql::QueryBuilder.new 'yahoo.finance.historicaldata'
-      query.select = 'date, Open, High, Low, Volume, Adj_close'
-      yql.query = query
-      
-      duration_examined.times do
-        break if res.length == 0
-        startDate = endDate - 1.year
-        query.conditions = { 
-          :symbol => ticker, 
-          :startDate => "#{startDate.year}-#{startDate.month}-#{startDate.day}", 
-          :endDate => "#{endDate.year}-#{endDate.month}-#{endDate.day}" 
-        }
-        res = JSON.parse(yql.get.show)["query"]["results"]["quote"]
-        daily_data += res
-        endDate = endDate - 1.year
+  def get_prices(ticker, duration_examined)
+    yql = Yql::Client.new
+    yql.format = "json"
+    daily_data = [] #daily prices IPO - now
+    res = [0] #temp holder for year's prices
+    endDate = DateTime.now
+    query = Yql::QueryBuilder.new 'yahoo.finance.historicaldata'
+    query.select = 'date, Open, High, Low, Volume, Adj_close'
+    yql.query = query
+    
+    duration_examined.times do
+      break if res.length == 0
+      startDate = endDate - 1.year
+      query.conditions = { 
+        :symbol => ticker, 
+        :startDate => "#{startDate.year}-#{startDate.month}-#{startDate.day}", 
+        :endDate => "#{endDate.year}-#{endDate.month}-#{endDate.day}" 
+      }
+      res = JSON.parse(yql.get.show)["query"]["results"]["quote"]
+      daily_data += res
+      endDate = endDate - 1.year
+    end
+    daily_data.reverse
+  end
+
+  def find_ranges(ticker, change, duration, duration_examined, length = 1)
+    days = [] #days that trigger entry signal
+    daily_data = get_prices(ticker, duration_examined)
+    decrease = change < 0 ? true : false
+    
+    daily_data.each_with_index do |day_data, idx|
+      start = day_data
+      end_d = daily_data[idx + duration - 1]
+      break unless end_d
+      max_incr = (end_d["High"].to_f - start["Low"].to_f) / end_d["High"].to_f
+      max_decr = (end_d["Low"].to_f - start["High"].to_f) / end_d["Low"].to_f
+
+      if decrease && max_decr <= change ||
+        !decrease && max_incr >= change
+        days << end_d
       end
-      daily_data
     end
 
-    def find_ranges(ticker, change, duration, length = 0, duration_examined = 10)
-      days = [] #days that trigger entry signal
-      daily_data = get_prices(ticker, duration_examined)
-      
-      daily_data.each_with_index do |day_data, idx|
-        start = day_data
-        end_d = daily_data[idx + duration - 1]
-        if end_d && (start["High"].to_f - end_d["Low"].to_f) / start["High"].to_f <= change ||
-          end_d && (start["Low"].to_f - end_d["High"].to_f) / start["Low"].to_f >= change
-          days << DateTime.new(*end_d["date"].split("-").map(&:to_i))
+    if length > 1
+      successive_days = []
+      (days.length - 1).times do |idx|
+        start = days[idx]["date"]
+        count = 0
+        end_d = days[idx + 1 + count]["date"]
+        while self.get_successive_days(start, end_d, duration) &&
+          count < length
+          count += 1
+          end_d = days[idx + 1 + count]["date"]
         end
+        successive_days << days[idx] if count == length - 1
       end
-
-      days
+      return successive_days
     end
+    days
+  end
 
-
+  def get_successive_days(start_d, end_d, duration)
+    # look through each day in days. if hit a string
+    # of length successive days spaced by duration,
+    # extract that part and return
+    start_date = DateTime.new(*start_d.split("-").map(&:to_i))
+    end_date = DateTime.new(*end_d.split("-").map(&:to_i))
+    return true if end_date - start_date == duration
+    false
   end
 
 end
