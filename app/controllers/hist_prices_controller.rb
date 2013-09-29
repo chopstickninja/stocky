184e9cba198f41c1392f1ade4cdb7671f39862c9
Ted Cheng <yiqun.cheng@gmail.com>
2013-09-29 12:19:57 -0700

    works

--- app/controllers/hist_prices_controller.rb
+++ app/controllers/hist_prices_controller.rb
@@ -1,11 +1,11 @@
 class HistPricesController < ApplicationController
   def index
     @prices = [
-      {date: 09/23/13, price: 30.26},
-      {date: 09/24/13, price: 31.27},
-      {date: 09/25/13, price: 31.34},
-      {date: 09/26/13, price: 32.75},
-      {date: 09/27/13, price: 33.55}]
+      {date: "09/23/13", price: 30.26},
+      {date: "09/24/13", price: 31.27},
+      {date: "09/25/13", price: 31.34},
+      {date: "09/26/13", price: 32.75},
+      {date: "09/27/13", price: 33.55}]
     render :json => @prices
   end
 end
