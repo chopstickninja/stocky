1e7b3d73254e10f20a56b37c11527134ef6c7936
Jenny Chou <j.3h15u@gmail.com>
2013-09-29 03:50:12 -0700

    todo: find_ranges with length and filter queries/callbacks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,12 +7,12 @@ module ApplicationHelper
     # =>
     # when ticker change by increment per duration for length
   
-  class parser
+  class Parser
 
     def get_prices(ticker, duration_examined)
       yql = Yql::Client.new
       yql.format = "json"
-      daily_data = [[]] #daily prices IPO - now
+      daily_data = [] #daily prices IPO - now
       res = [0] #temp holder for year's prices
       endDate = DateTime.now
       query = Yql::QueryBuilder.new 'yahoo.finance.historicaldata'
@@ -27,28 +27,29 @@ module ApplicationHelper
           :startDate => "#{startDate.year}-#{startDate.month}-#{startDate.day}", 
           :endDate => "#{endDate.year}-#{endDate.month}-#{endDate.day}" 
         }
-        res = JSON.parse(yql.get.show)["query"]["results"]["quotes"]
+        res = JSON.parse(yql.get.show)["query"]["results"]["quote"]
         daily_data += res
         endDate = endDate - 1.year
       end
       daily_data
     end
 
-    def find_ranges(ticker, change, duration, length, duration_examined = 10)
+    def find_ranges(ticker, change, duration, length = 0, duration_examined = 10)
       days = [] #days that trigger entry signal
-      prices = get_prices(ticker, duration_examined)
+      daily_data = get_prices(ticker, duration_examined)
       
       daily_data.each_with_index do |day_data, idx|
         start = day_data
         end_d = daily_data[idx + duration - 1]
-        if start["High"] - end_d["Low"] <= change ||
-          start["Low"] - end_d["High"] >= change
-          days << DateTime.new(*end_d.split("-").map(&:to_i))
+        if end_d && (start["High"].to_f - end_d["Low"].to_f) / start["High"].to_f <= change ||
+          end_d && (start["Low"].to_f - end_d["High"].to_f) / start["Low"].to_f >= change
+          days << DateTime.new(*end_d["date"].split("-").map(&:to_i))
         end
       end
 
       days
     end
+    puts find_ranges("GOOG", 0.01, 10, 180)
   end
 end
       #   b. redis
