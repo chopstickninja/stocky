1832ad473c8f3bd5f1ad6ea80e7525ed97861680
John Wahba <john.a.wahba@gmail.com>
2013-09-29 10:30:26 -0700

    integrate parser

--- script/test.rb
+++ script/test.rb
@@ -1,8 +1,7 @@
 p "hello!!!"
-query = "where AAPL increases by 5% in 1 day use 3% of portfolio to buy AAPL."
-callback = "where AAPL increases by 5% in 3 days or AAPL decreases by 10% in 3 days exit"
+query = "when AAPL increases by 5% in 1 day use 3% of portfolio to buy AAPL."
+callback = "when AAPL decreases by 3% in 1 days exit"
 duration_examined = 1
-max_open_trades = 10
+max_open_trades = 1
 
-p obj = Parser.new(query, callback, duration_examined,max_open_trades)
-p 'hello'
+p obj = ApplicationHelper::Parser.new(query, callback, duration_examined,max_open_trades)
